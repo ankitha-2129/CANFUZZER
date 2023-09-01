7a2cfa29a4073c293937f232140a42dbde6c73c8
Ankitha Gowda <ankithagowda2129@gmail.com>
2023-09-01 03:41:42 -0400

    sh updated

--- canfuzzer.sh
+++ canfuzzer.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
-echo "Checking the dependencies please wait......Do not exit"
-pip3 install -r requirements.txt
-echo "Done checking dependencies"
 sudo ip link set can0 up type can bitrate 250000
-echo "CAN Interface up with 250000 baudrate"
-echo "Downloading CANFuzzer"
-git clone https://github.com/ankitha-2129/CANFUZZER.git
-echo "Initializing GUI"
-python3 CANFUZZER/canlinuxgui.py
+# Run your command and capture its exit status
+your_command_status=$?
+
+if [ $your_command_status -eq 0 ]; then
+    echo "CAN Interface up with 250000 baudrate"
+    folder_name="CANFUZZER"
+    if [ -d "$folder_name" ]; then
+        echo "Repo Already Cloned"
+        echo "Checking the dependencies please wait......Do not exit"
+        pip3 install -r CANFUZZER/requirements.txt
+        echo "Done checking dependencies"
+        echo "Initializing GUI"
+        python3 CANFUZZER/canlinuxgui.py  
+    else
+        echo "Downloading CANFuzzer"
+        git clone https://github.com/ankitha-2129/CANFUZZER.git
+        echo "Checking the dependencies please wait......Do not exit"
+        pip3 install -r CANFUZZER/requirements.txt
+        echo "Done checking dependencies"
+        echo "Initializing GUI"
+        python3 CANFUZZER/canlinuxgui.py
+    fi
+else
+    echo "Command failed with exit status $your_command_status."
+    echo "Please connect PCAN USB properly"
+fi
