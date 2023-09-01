ef5ed8caec30264661590cd763ddb4ac4d94649f
Ankitha Gowda <ankithagowda2129@gmail.com>
2023-09-01 03:13:08 -0400

    .sh file echo changes are made

--- canfuzzer.sh
+++ canfuzzer.sh
@@ -4,5 +4,7 @@ pip3 install -r requirements.txt
 echo "Done checking dependencies"
 sudo ip link set can0 up type can bitrate 250000
 echo "CAN Interface up with 250000 baudrate"
+echo "Downloading CANFuzzeriyguyfuyf8"
+git clone https://github.com/ankitha-2129/CANFUZZER.git
 echo "Initializing GUI"
-python3 /home/kali/Desktop/CANFUZZEREXE/canlinuxgui.py
+python3 canlinuxgui.py
